48570675b2afe55fc9d9b4b190ea33e7c9b9ce39
trungkiendev203 <trungkiendev203@gmail.com>
2026-08-11 11:31:03 +0700

    Update site font family to DM Serif Display for high-end luxury editorial typography

--- views/layouts/main.php
+++ views/layouts/main.php
@@ -18,10 +18,9 @@
   <meta property="og:url" content="<?= e($seo['canonical'] ?? base_url()) ?>">
   <meta property="og:type" content="website">
 
-  <!-- Self-hosted font preloading -->
   <link rel="preconnect" href="https://fonts.googleapis.com">
   <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-  <link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,300;0,400;0,500;0,700;0,900;1,400&family=Playfair+Display:ital,wght@0,600;0,700;1,400&display=swap" rel="stylesheet">
+  <link href="https://fonts.googleapis.com/css2?family=DM+Serif+Display:ital@0;1&family=Plus+Jakarta+Sans:wght@400;500;600;700;800&family=Inter:wght@400;500;600;700&display=swap" rel="stylesheet">
 
   <link rel="stylesheet" href="<?= asset('assets/css/main.css') ?>">
   <link rel="stylesheet" href="<?= asset('assets/css/components.css') ?>">
